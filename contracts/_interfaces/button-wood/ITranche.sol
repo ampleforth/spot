3515628b95a8540b56e7cb0ca1d14d54357b9709
aalavandhan1984 <6264334+aalavandhan@users.noreply.github.com>
2022-02-28 16:41:14 -0500

    Perp contract refactor

--- contracts/_interfaces/button-wood/ITranche.sol
+++ contracts/_interfaces/button-wood/ITranche.sol
@@ -1,8 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // solhint-disable-next-line compiler-version
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface ITranche is IERC20 {
-    function collateralToken() external view returns (address);
-
     function bond() external view returns (address);
 }
