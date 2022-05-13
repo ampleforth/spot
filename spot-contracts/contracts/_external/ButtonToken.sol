cfca9b56b72e7137b9237f88a924b6d4647afb8f
nithin <nithin@fragments.org>
2022-05-13 09:53:49 -0700

    updated compiler version

--- spot-contracts/contracts/_external/ButtonToken.sol
+++ spot-contracts/contracts/_external/ButtonToken.sol
@@ -1,3 +1,3 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.3;
 
 import { ButtonToken } from "button-wrappers/contracts/ButtonToken.sol";
--- spot-contracts/contracts/_utils/AddressQueueHelpers.sol
+++ spot-contracts/contracts/_utils/AddressQueueHelpers.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 /// @notice Expected item to not be `address(0)`.
 error InvalidItem();
