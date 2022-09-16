3b89a14db6ab375b0f8e4c78d1d9ca9ef29cd7c8
aalavandhan1984 <6264334+aalavandhan@users.noreply.github.com>
2022-09-16 11:21:08 -0400

    bumped up hardhat and solidity versions

--- spot-contracts/contracts/strategies/UnitPricingStrategy.sol
+++ spot-contracts/contracts/strategies/UnitPricingStrategy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.17;
 
 import { IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import { ITranche } from "../_interfaces/buttonwood/ITranche.sol";
