0174cd5288968c5e5e91f2052057339a6e8c8ff9
aalavandhan1984 <6264334+aalavandhan@users.noreply.github.com>
2022-09-20 13:54:19 -0400

    ran linter

--- spot-contracts/contracts/test/mocks/MockTranche.sol
+++ spot-contracts/contracts/test/mocks/MockTranche.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.15;
 
-import {MockERC20} from "./MockERC20.sol";
+import { MockERC20 } from "./MockERC20.sol";
 
 contract MockTranche is MockERC20 {
     address public bond;
