95c02c036fa4458de8e188179220356f782e6968
aalavandhan1984 <6264334+aalavandhan@users.noreply.github.com>
2023-04-10 09:46:00 -0400

    bumped up compiler version

--- spot-contracts/contracts/test/mocks/MockPricingStrategy.sol
+++ spot-contracts/contracts/test/mocks/MockPricingStrategy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 contract MockPricingStrategy {
     uint8 private _decimals;
--- spot-contracts/hardhat.config.ts
+++ spot-contracts/hardhat.config.ts
@@ -46,7 +46,7 @@ export default {
   solidity: {
     compilers: [
       {
-        version: "0.8.18",
+        version: "0.8.19",
         settings: {
           optimizer: {
             enabled: true,
