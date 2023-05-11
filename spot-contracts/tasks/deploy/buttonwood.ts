d212d794adbe52cffcfc96706cb690a67411d97c
nms-7 <nixon.ubud@gmail.com>
2023-05-10 17:21:36 -0700

    updates deployment config / scripts for local ganache

--- spot-contracts/tasks/deploy/buttonwood.ts
+++ spot-contracts/tasks/deploy/buttonwood.ts
@@ -5,54 +5,60 @@ import { getContractFactoryFromExternalArtifacts } from "../helpers";
 
 const DUMMY_ADDRESS = "0x000000000000000000000000000000000000dead";
 
-task("deploy:BondFactory").setAction(async function (_args: TaskArguments, hre) {
-  console.log("Signer", await (await hre.ethers.getSigners())[0].getAddress());
-  const BondController = await getContractFactoryFromExternalArtifacts(hre.ethers, "BondController");
-  const bondController = await BondController.deploy();
-  await bondController.deployed();
-  console.log("Bond controller", bondController.address);
+task("deploy:BondFactory")
+  .addParam("verify", "flag to set false for local deployments", true, types.boolean)
+  .setAction(async function (args: TaskArguments, hre) {
+    console.log("Signer", await (await hre.ethers.getSigners())[0].getAddress());
+    const BondController = await getContractFactoryFromExternalArtifacts(hre.ethers, "BondController");
+    const bondController = await BondController.deploy();
+    await bondController.deployed();
+    console.log("Bond controller", bondController.address);
 
-  const Tranche = await getContractFactoryFromExternalArtifacts(hre.ethers, "Tranche");
-  const tranche = await Tranche.deploy();
-  await tranche.deployed();
-  console.log("Tranche", tranche.address);
+    const Tranche = await getContractFactoryFromExternalArtifacts(hre.ethers, "Tranche");
+    const tranche = await Tranche.deploy();
+    await tranche.deployed();
+    console.log("Tranche", tranche.address);
 
-  const TrancheFactory = await getContractFactoryFromExternalArtifacts(hre.ethers, "TrancheFactory");
-  const trancheFactory = await TrancheFactory.deploy(tranche.address);
-  await trancheFactory.deployed();
-  console.log("Tranche Factory", trancheFactory.address);
+    const TrancheFactory = await getContractFactoryFromExternalArtifacts(hre.ethers, "TrancheFactory");
+    const trancheFactory = await TrancheFactory.deploy(tranche.address);
+    await trancheFactory.deployed();
+    console.log("Tranche Factory", trancheFactory.address);
 
-  await tranche["init(string,string,address,address)"]("IMPLEMENTATION", "IMPL", DUMMY_ADDRESS, DUMMY_ADDRESS);
-  await bondController.init(
-    trancheFactory.address,
-    tranche.address,
-    DUMMY_ADDRESS,
-    [200, 300, 500],
-    hre.ethers.constants.MaxUint256,
-    0,
-  );
+    await tranche["init(string,string,address,address)"]("IMPLEMENTATION", "IMPL", DUMMY_ADDRESS, DUMMY_ADDRESS);
+    await bondController.init(
+      trancheFactory.address,
+      tranche.address,
+      DUMMY_ADDRESS,
+      [200, 300, 500],
+      hre.ethers.constants.MaxUint256,
+      0,
+    );
 
-  const BondFactory = await getContractFactoryFromExternalArtifacts(hre.ethers, "BondFactory");
-  const bondFactory = await BondFactory.deploy(bondController.address, trancheFactory.address);
-  await bondFactory.deployed();
-  console.log("Bond Factory", bondFactory.address);
+    const BondFactory = await getContractFactoryFromExternalArtifacts(hre.ethers, "BondFactory");
+    const bondFactory = await BondFactory.deploy(bondController.address, trancheFactory.address);
+    await bondFactory.deployed();
+    console.log("Bond Factory", bondFactory.address);
 
-  try {
-    await hre.run("verify:Template", { address: bondController.address });
-    await hre.run("verify:Template", { address: tranche.address });
-    await hre.run("verify:TrancheFactory", {
-      address: trancheFactory.address,
-      template: tranche.address,
-    });
-    await hre.run("verify:BondFactory", {
-      address: bondFactory.address,
-      template: bondController.address,
-      trancheFactory: trancheFactory.address,
-    });
-  } catch (e) {
-    console.log("Unable to verify on etherscan", e);
-  }
-});
+    if (args.verify) {
+      try {
+        await hre.run("verify:Template", { address: bondController.address });
+        await hre.run("verify:Template", { address: tranche.address });
+        await hre.run("verify:TrancheFactory", {
+          address: trancheFactory.address,
+          template: tranche.address,
+        });
+        await hre.run("verify:BondFactory", {
+          address: bondFactory.address,
+          template: bondController.address,
+          trancheFactory: trancheFactory.address,
+        });
+      } catch (e) {
+        console.log("Unable to verify on etherscan", e);
+      }
+    } else {
+      console.log("Skipping verification");
+    }
+  });
 
 task("verify:Template", "Verifies on etherscan")
   .addParam("address", "the contract address", undefined, types.string, false)
--- spot-contracts/tasks/ganache.sh
+++ spot-contracts/tasks/ganache.sh
@@ -1,10 +1,15 @@
-yarn run ganache-cli -s 123
-
 ########################################################################
-## DEPLOYMENT
-yarn hardhat --network ganache deploy:MockAMPL
 
-yarn hardhat --network ganache deploy:BondFactory
+## COMPILE
+yarn hardhat compile
+
+## DEPLOYMENT
+
+yarn hardhat --network ganache deploy:MockAMPL \
+	--verify "false"
+
+yarn hardhat --network ganache deploy:BondFactory \
+	--verify "false"
 
 yarn hardhat --network ganache deploy:BondIssuer \
   --bond-factory-address "0x25a02122Cd77FeB7981b6224b470111A8FA479F4" \
@@ -12,52 +17,40 @@ yarn hardhat --network ganache deploy:BondIssuer \
   --issue-frequency "60" \
   --issue-window-offset "0" \
   --collateral-token-address "0x00404F73C76BC75b0D86F8AdDA8500e987BF8232" \
-  --tranche-ratios "[500,500]"
+  --tranche-ratios "[500,500]" \
+  --verify "false"
 
 yarn hardhat --network ganache deploy:PerpetualTranche \
   --bond-issuer-address "0xeb289644a33df897B1E30f0aa5cC0F17DD29Bdc2" \
   --collateral-token-address "0x00404F73C76BC75b0D86F8AdDA8500e987BF8232" \
   --name "SPOT" \
   --symbol "SPOT" \
-  --pricing-strategy-ref "CDRPricingStrategy"
+  --verify "false"
 
 yarn hardhat --network ganache deploy:DiscountStrategy:setDiscount \
-  --discount-strategy-address "0x4741f9c161003100fF0Ba1097E149d143458bD0B" \
+  --discount-strategy-address "0xeEaC7F8841B8E4Aa4D9E63164227a4788dF6dC99" \
   --collateral-token-address "0x00404F73C76BC75b0D86F8AdDA8500e987BF8232" \
   --tranche-ratios "[500,500]" \
   --tranche-index "0" \
   --tranche-discount "1.0"
 
-yarn hardhat --network ganache deploy:Router
+yarn hardhat --network ganache ops:perp:updateTolerableTrancheMaturity \
+	--address "0xC090cFC721ea0624A30BE6796A53CE1EEf703D67" \
+	--minimum "500" \
+	--maximum "1000"
+
+yarn hardhat --network ganache deploy:Router \
+	--verify "false"
+
+yarn hardhat --network ganache deploy:RolloverVault \
+    --name "SPOT Rollover Vault Note" \
+    --symbol "SPOT-RV-NOTE" \
+    --perp-address "0xC090cFC721ea0624A30BE6796A53CE1EEf703D67" \
+	--verify "false"
 
 ########################################################################
 ## OPS
-
-yarn hardhat --network ganache ops:perp:info 0x89967625335C35c5FE1F3C1c03D37fdEb6f415Ed
-
-yarn hardhat --network ganache ops:trancheAndDeposit \
-  --router-address 0x4a57d51af3a8a90905a5F756E0B28cC2888A1bD5 \
-  --perp-address 0x89967625335C35c5FE1F3C1c03D37fdEb6f415Ed \
-  --collateral-amount 200
-
-yarn hardhat --network ganache ops:redeem \
-  --router-address 0x4a57d51af3a8a90905a5F756E0B28cC2888A1bD5 \
-  --perp-address 0x89967625335C35c5FE1F3C1c03D37fdEb6f415Ed \
-  --amount 10
-
-yarn hardhat --network ganache ops:redeemTranches \
-  --bond-issuer-address 0xeb289644a33df897B1E30f0aa5cC0F17DD29Bdc2 
-
-yarn hardhat --network ganache ops:trancheAndRollover \
-  --router-address 0x4a57d51af3a8a90905a5F756E0B28cC2888A1bD5 \
-  --perp-address 0x89967625335C35c5FE1F3C1c03D37fdEb6f415Ed \
-  --collateral-amount 200
-
 yarn hardhat --network ganache ops:increaseTimeBy 300
-yarn hardhat --network ganache ops:updateState 0x89967625335C35c5FE1F3C1c03D37fdEb6f415Ed
+yarn hardhat --network ganache ops:updateState 0xC090cFC721ea0624A30BE6796A53CE1EEf703D67 
+yarn hardhat --network ganache ops:perp:info 0xC090cFC721ea0624A30BE6796A53CE1EEf703D67
 
-yarn hardhat --network ganache ops:rebase:MockAMPL \
-  --ampl-address "0x00404F73C76BC75b0D86F8AdDA8500e987BF8232" \
-  --rebase-perc 0.05
-
-########################################################################
