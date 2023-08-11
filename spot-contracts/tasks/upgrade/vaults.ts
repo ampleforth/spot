e43b8f78df713ff11858ff0838b5d67dfaf8e903
aalavandhann <6264334+aalavandhan@users.noreply.github.com>
2023-08-11 15:13:04 -0500

    vault upgrade linedup

--- spot-contracts/tasks/upgrade/vaults.ts
+++ spot-contracts/tasks/upgrade/vaults.ts
@@ -1,4 +1,4 @@
-import { getImplementationAddress } from "@openzeppelin/upgrades-core";
+import { getAdminAddress, getImplementationAddress } from "@openzeppelin/upgrades-core";
 import { task, types } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 import { sleep } from "../helpers";
@@ -21,6 +21,32 @@ task("upgrade:rolloverVault:testnet")
 
     const newImpl = await getImplementationAddress(hre.ethers.provider, vaultAddress);
     console.log("Updated implementation", newImpl);
+
+    await sleep(15);
+    await hre.run("verify:contract", {
+      address: newImpl,
+    });
+  });
+
+
+task("prepare_upgrade:rolloverVault:mainnet")
+  .addPositionalParam("vaultAddress", "the address of the perp contract", undefined, types.string, false)
+  .addParam("fromIdx", "the index of sender", 0, types.int)
+  .setAction(async function (args: TaskArguments, hre) {
+    const { vaultAddress } = args;
+
+    const signer = (await hre.ethers.getSigners())[args.fromIdx];
+    const signerAddress = await signer.getAddress();
+    console.log("Signer", signerAddress);
+
+    console.log("Proxy Admin", await getAdminAddress(hre.ethers.provider, vaultAddress));
+
+    const RolloverVault = await hre.ethers.getContractFactory("RolloverVault");
+    const newImpl = await hre.upgrades.prepareUpgrade(vaultAddress, RolloverVault);
+    console.log("New implementation at:", newImpl);
+
+    console.log("Update implementation through the multisig");
+    console.log(`proxyAdmin.upgrade(${vaultAddress}, ${newImpl})`);
 
     await sleep(15);
     await hre.run("verify:contract", {
