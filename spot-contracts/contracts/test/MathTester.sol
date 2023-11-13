f66b9db7a05d99f8d551508b7076eb741c0f491a
aalavandhan1984 <6264334+aalavandhan@users.noreply.github.com>
2023-11-13 13:41:22 -0500

    removed safemath dependency for sigmoid computation, imported math lib code from amppleforth core

--- spot-contracts/contracts/test/MathTester.sol
+++ spot-contracts/contracts/test/MathTester.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.19;
 
-import { SignedMathHelpers } from "../_utils/SignedMathHelpers.sol";
 import { Sigmoid } from "../_utils/Sigmoid.sol";
 
 contract MathTester {
-    using SignedMathHelpers for int256;
-
-    function sign(int256 a) public pure returns (int256) {
-        return a.sign();
+    function twoPower(int256 exp, int256 one) public pure returns (int256) {
+        return Sigmoid.twoPower(exp, one);
     }
 
     function compute(
