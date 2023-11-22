4dcb04ac3501b27f46c10af66a61d7769ff62766
aalavandhann <6264334+aalavandhan@users.noreply.github.com>
2023-11-22 11:45:45 -0500

    imported two pow tests
    Signed-off-by: aalavandhann <6264334+aalavandhan@users.noreply.github.com>

--- spot-contracts/test/_utils/Sigmoid.ts
+++ spot-contracts/test/_utils/Sigmoid.ts
@@ -1,10 +1,10 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Contract } from "ethers";
+import { Contract, BigNumber } from "ethers";
 
 import { toPercFixedPtAmt } from "../helpers";
 
-describe("Sigmoid", function () {
+describe.only("Sigmoid", function () {
   let math: Contract;
   async function cmp(x, y, lower, upper, growth) {
     expect(
@@ -47,6 +47,80 @@ describe("Sigmoid", function () {
       await cmp(3, 0.04885057, -0.01, 0.05, 4);
       await cmp(4, 0.04992684, -0.01, 0.05, 4);
       await cmp(10, 0.05, -0.01, 0.05, 4);
+    });
+  });
+
+  describe("twoPower", function () {
+    before(async function () {
+      const MathTester = await ethers.getContractFactory("MathTester");
+      math = await MathTester.deploy();
+      await math.deployed();
+    });
+
+    const decimals18 = BigNumber.from("1000000000000000000");
+    const decimals10 = BigNumber.from("10000000000");
+    it("2^0", async function () {
+      const e = BigNumber.from(0);
+      const one = BigNumber.from(1).mul(decimals18);
+      expect(await math.twoPower(e, one)).to.eq(one);
+    });
+    it("2^1", async function () {
+      const e = BigNumber.from(1).mul(decimals18);
+      const one = BigNumber.from(1).mul(decimals18);
+      const result = BigNumber.from(2).mul(decimals18);
+      expect(await math.twoPower(e, one)).to.eq(result);
+    });
+    it("2^30", async function () {
+      const e = BigNumber.from(30).mul(decimals18);
+      const one = BigNumber.from(1).mul(decimals18);
+      const result = BigNumber.from(2 ** 30).mul(decimals18);
+      expect(await math.twoPower(e, one)).to.eq(result);
+    });
+    it("2^2.5", async function () {
+      const e = BigNumber.from("25000000000");
+      const one = BigNumber.from(1).mul(decimals10);
+      const result = BigNumber.from("56568542494");
+      expect(await math.twoPower(e, one)).to.eq(result);
+    });
+    it("2^2.25", async function () {
+      const e = BigNumber.from("22500000000");
+      const one = BigNumber.from(1).mul(decimals10);
+      const result = BigNumber.from("47568284600");
+      expect(await math.twoPower(e, one)).to.eq(result);
+    });
+    it("2^-2.25", async function () {
+      const e = BigNumber.from("-22500000000");
+      const one = BigNumber.from(1).mul(decimals10);
+      const result = BigNumber.from("2102241038");
+      expect(await math.twoPower(e, one)).to.eq(result);
+    });
+    it("2^-0.6", async function () {
+      const e = BigNumber.from("-6000000000");
+      const one = BigNumber.from(1).mul(decimals10);
+      const result = BigNumber.from("6626183216");
+      expect(await math.twoPower(e, one)).to.eq(result);
+    });
+    it("2^2.96875", async function () {
+      const e = BigNumber.from("29687500000");
+      const one = BigNumber.from(1).mul(decimals10);
+      const result = BigNumber.from("78285764964");
+      expect(await math.twoPower(e, one)).to.eq(result);
+    });
+    it("2^2.99", async function () {
+      const e = BigNumber.from("29900000000");
+      const one = BigNumber.from(1).mul(decimals10);
+      const result = BigNumber.from("78285764964");
+      expect(await math.twoPower(e, one)).to.eq(result);
+    });
+    it("should fail on too small exponents", async function () {
+      const e = BigNumber.from("-1011000000000");
+      const one = BigNumber.from(1).mul(decimals10);
+      await expect(math.twoPower(e, one)).to.be.revertedWith("Sigmoid: twoPower exp too big");
+    });
+    it("should fail on too large exponents", async function () {
+      const e = BigNumber.from("1011000000000");
+      const one = BigNumber.from(1).mul(decimals10);
+      await expect(math.twoPower(e, one)).to.be.revertedWith("Sigmoid: twoPower exp too big");
     });
   });
 });
