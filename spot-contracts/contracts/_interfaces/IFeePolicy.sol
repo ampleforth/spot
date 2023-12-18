37e57b1bea99c885b2bc9d3a25af5b3db6210794
aalavandhann <6264334+aalavandhan@users.noreply.github.com>
2023-12-18 09:27:57 -0500

    subscription aware fee policy for swapping, using minting bond as the source of truth for tranche ratios

--- spot-contracts/contracts/_interfaces/IFeePolicy.sol
+++ spot-contracts/contracts/_interfaces/IFeePolicy.sol
@@ -5,18 +5,19 @@ import { IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC
 import { IBondController } from "./buttonwood/IBondController.sol";
 
 interface IFeePolicy {
-    /// @notice The percentage of the mint perp tokens to be charged as fees,
-    ///         as a fixed-point number with {PRICE_DECIMAL} decimal places.
-    function computePerpMintFeePerc() external returns (uint256);
+    /// @param valueIn The value of incoming assets used to mint perps.
+    /// @return The percentage of the mint perp tokens to be charged as fees,
+    ///         as a fixed-point number with {DECIMALS} decimal places.
+    function computePerpMintFeePerc(uint256 valueIn) external returns (uint256);
 
-    /// @notice The percentage of the burnt perp tokens to be charged as fees,
-    ///         as a fixed-point number with {PRICE_DECIMAL} decimal places.
+    /// @return The percentage of the burnt perp tokens to be charged as fees,
+    ///         as a fixed-point number with {DECIMALS} decimal places.
     function computePerpBurnFeePerc() external returns (uint256);
 
-    /// @notice The applied exchange rate adjustment between tranches into perp and
+    /// @return The applied exchange rate adjustment between tranches into perp and
     ///         tokens out of perp during a rollover,
-    ///         as a fixed-point number with {PRICE_DECIMAL} decimal places.
-    ///      - A fee of 0%, implies the rollover exchange rate is unaltered.
+    ///         as a fixed-point number with {DECIMALS} decimal places.
+    /// @dev - A fee of 0%, implies the rollover exchange rate is unaltered.
     ///         example) 100 tranchesIn for 100 tranchesOut
     ///      - A fee of 1%, implies the exchange rate is adjusted in favor of tranchesIn.
     ///         example) 100 tranchesIn for 99 tranchesOut
@@ -24,18 +25,40 @@ interface IFeePolicy {
     ///         example) 99 tranchesIn for 100 tranchesOut
     function computePerpRolloverFeePerc() external returns (int256);
 
-    /// @notice The percentage of the mint vault note amount to be charged as fees,
-    ///         as a fixed-point number with {PRICE_DECIMAL} decimal places.
+    /// @return The percentage of the mint vault note amount to be charged as fees,
+    ///         as a fixed-point number with {DECIMALS} decimal places.
     function computeVaultMintFeePerc() external returns (uint256);
 
-    /// @notice The percentage of the burnt vault note amount to be charged as fees,
-    ///         as a fixed-point number with {PRICE_DECIMAL} decimal places.
+    /// @return The percentage of the burnt vault note amount to be charged as fees,
+    ///         as a fixed-point number with {DECIMALS} decimal places.
     function computeVaultBurnFeePerc() external returns (uint256);
 
-    /// @notice The fixed amount fee charged by the vault during each deployment,
+    /// @return The fixed amount fee charged by the vault during each deployment,
     ///         denominated in the underlying collateral asset.
     function computeVaultDeploymentFee() external returns (uint256);
 
-    /// @notice Number of decimals representing a multiplier of 1.0. So, 100% = 1*10**decimals
+    /// @param valueIn The value of underlying tokens swapped in.
+    /// @return The percentage of perp tokens out to be charged as swap fee by the perp and vault systems,
+    ///         as a fixed-point numbers with {DECIMALS} decimal places.
+    function computeUnderlyingToPerpSwapFeePercs(uint256 valueIn) external returns (uint256, uint256);
+
+    /// @return The percentage of underlying tokens out to be charged as swap fee by the perp and vault systems,
+    ///         as a fixed-point numbers with {DECIMALS} decimal places.
+    function computePerpToUnderlyingSwapFeePercs() external returns (uint256, uint256);
+
+    /// @return Number of decimals representing 1.0 or 100%.
     function decimals() external view returns (uint8);
+
+    /// @dev The subscription state the vault system relative to the perp supply.
+    struct SubscriptionState {
+        /// @notice The recorded tvl of perp.
+        uint256 perpTVL;
+        /// @notice The recorded tvl of the rollover vault.
+        uint256 vaultTVL;
+        /// @notice Computed normalized subscription ratio.
+        uint256 normalizedSubscriptionRatio;
+    }
+
+    /// @dev Computes the current subscription state of vault system.
+    function computeSubscriptionState() external returns (SubscriptionState memory);
 }
