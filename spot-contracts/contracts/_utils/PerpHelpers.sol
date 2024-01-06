720dfd2c9c71cd3b77f1d199107e618e56dd1673
aalavandhann <6264334+aalavandhan@users.noreply.github.com>
2024-01-06 08:47:27 -0500

    Vault clean up, function reorg and optimized tvl comp

---
+++ spot-contracts/contracts/_utils/PerpHelpers.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.19;
+
+import { IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import { IBondController } from "../_interfaces/buttonwood/IBondController.sol";
+import { ITranche } from "../_interfaces/buttonwood/ITranche.sol";
+import { IPerpetualTranche } from "../_interfaces/IPerpetualTranche.sol";
+
+import { MathUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
+import { BondHelpers } from "./BondHelpers.sol";
+
+/**
+ *  @title PerpHelpers
+ *
+ *  @notice Library with helper functions for the Perpetual tranche contract.
+ *
+ */
+library PerpHelpers {
+    using MathUpgradeable for uint256;
+    using BondHelpers for IBondController;
+
+    /// @notice The function estimates the amount of underlying tokens that need to be tranched
+    ///         in order to mint the given amount of perp tokens.
+    /// @dev This function is guaranteed to over-estimate, ie) when you tranche the estimated amount
+    ///      of underlying tokens, and use the senior tranches to mint perps,
+    ///      you might end up minting slightly more than `perpAmtToMint`.
+    /// @param perp The address of the perp contract.
+    /// @param perpTVL The current TVL of perp.
+    /// @param perpAmtToMint The required number of perp tokens to mint.
+    /// @return underylingAmtToTranche The number of underlying tokens to tranche.
+    /// @return seniorAmtToDeposit The number of minted seniors to then deposit into perp.
+    /// @return depositBond The current deposit bond.
+    /// @return depositTranche The current deposit tranche.
+    function estimateUnderlyingAmtToTranche(
+        IPerpetualTranche perp,
+        uint256 perpTVL,
+        uint256 perpAmtToMint
+    )
+        internal
+        returns (
+            uint256,
+            uint256,
+            IBondController,
+            ITranche
+        )
+    {
+        // We assume that:
+        //  - Perp only accepts the "most" senior tranche from the deposit bond.
+        //  - The deposit bond is NOT mature.
+        //  - No fees are withheld while tranching
+
+        // Get the minting bond and tranche data
+        IBondController depositBond = perp.getDepositBond();
+        ITranche depositTranche = depositBond.getSeniorTranche();
+        (uint256 seniorTR, uint256 juniorTR) = depositBond.getSeniorJuniorRatio();
+
+        uint256 bondCollateralBalance = IERC20Upgradeable(depositBond.collateralToken()).balanceOf(
+            address(depositBond)
+        );
+        uint256 seniorAmtToDeposit = _estimateDepositAmt(
+            perpTVL,
+            perp.totalSupply(),
+            depositTranche,
+            bondCollateralBalance,
+            perpAmtToMint
+        );
+
+        uint256 underlyingAmtToTranche = seniorAmtToDeposit
+            .mulDiv(bondCollateralBalance, depositBond.totalDebt(), MathUpgradeable.Rounding.Up)
+            .mulDiv((seniorTR + juniorTR), seniorTR, MathUpgradeable.Rounding.Up);
+
+        return (underlyingAmtToTranche, seniorAmtToDeposit, depositBond, depositTranche);
+    }
+
+    /// @notice This function estimates the amount of tranche tokens that need to be deposited
+    ///         to mint the given number of perp tokens.
+    /// @dev This function is guaranteed to over-estimate, ie) when you deposit the estimated amount
+    ///      of tranche tokens tokens, you might end up minting slightly more than `perpAmtToMint`.
+    ///      This function is an inverse of the `perp.computeMintAmt` function.
+    /// @param perpTVL The current TVL of perp.
+    /// @param perpSupply The total supply of perp tokens.
+    /// @param seniorTranche The most "senior" tranche of the deposit bond currently accepted by perp.
+    /// @param bondCollateralBalance The parent bond's collateral balance.
+    /// @param perpAmtToMint The required number of perp tokens to mint.
+    /// @return seniorAmtToDeposit The number of seniors to deposit into perp.
+    function _estimateDepositAmt(
+        uint256 perpTVL,
+        uint256 perpSupply,
+        ITranche seniorTranche,
+        uint256 bondCollateralBalance,
+        uint256 perpAmtToMint
+    ) private view returns (uint256) {
+        uint256 seniorSupply = seniorTranche.totalSupply();
+        uint256 seniorClaim = MathUpgradeable.min(seniorSupply, bondCollateralBalance);
+        return
+            perpAmtToMint.mulDiv(perpTVL, perpSupply, MathUpgradeable.Rounding.Up).mulDiv(
+                seniorSupply,
+                seniorClaim,
+                MathUpgradeable.Rounding.Up
+            );
+    }
+}
--- spot-contracts/contracts/_utils/TrancheHelpers.sol
+++ spot-contracts/contracts/_utils/TrancheHelpers.sol
@@ -17,17 +17,41 @@ import { BondHelpers } from "./BondHelpers.sol";
  *
  */
 library TrancheHelpers {
-    /// @notice Given a tranche, looks up the collateral balance backing the tranche supply.
-    /// @param t Address of the tranche token.
+    /// @notice Given a tranche, calculates the claimable collateral balance backing the tranche supply.
+    /// @param tranche Address of the tranche token.
+    /// @param bond Address of the tranche's parent bond.
+    /// @param collateralToken Address of the tranche's underlying collateral token.
     /// @return The collateral balance and the tranche token supply.
-    function getTrancheCollateralization(ITranche t) internal view returns (uint256, uint256) {
-        IBondController bond = IBondController(t.bond());
+    function getTrancheCollateralization(
+        ITranche tranche,
+        IBondController bond,
+        IERC20Upgradeable collateralToken
+    ) internal view returns (uint256, uint256) {
+        // When the tranche's parent bond is mature
+        if (bond.isMature()) {
+            uint256 trancheCollateralBalance = collateralToken.balanceOf(address(tranche));
+            uint256 trancheSupply_ = tranche.totalSupply();
+            return (trancheCollateralBalance, trancheSupply_);
+        }
+
+        // When the tranche's parent bond is not mature
         BondTranches memory bt = BondHelpers.getTranches(bond);
-        uint256[] memory collateralBalances;
-        uint256[] memory trancheSupplies;
-        (collateralBalances, trancheSupplies) = BondHelpers.getTrancheCollateralizations(bond, bt);
-        uint256 trancheIndex = BondTranchesHelpers.indexOf(bt, t);
-        return (collateralBalances[trancheIndex], trancheSupplies[trancheIndex]);
+
+        // NOTE: We assume that the system only accepts with bonds with 2 tranches
+        require(bt.tranches.length == 2, "TrancheHelpers: Unexpected tranche count");
+        uint256 trancheIndex = (bt.tranches[0] == tranche) ? 0 : 1;
+
+        uint256 bondCollateralBalance = collateralToken.balanceOf(address(bond));
+        uint256 trancheSupply = tranche.totalSupply();
+        uint256 trancheClaim = 0;
+        if (trancheIndex == 0) {
+            trancheClaim = MathUpgradeable.min(trancheSupply, bondCollateralBalance);
+        } else {
+            uint256 seniorSupply = bt.tranches[0].totalSupply();
+            uint256 seniorClaim = MathUpgradeable.min(seniorSupply, bondCollateralBalance);
+            trancheClaim = bondCollateralBalance - seniorClaim;
+        }
+        return (trancheClaim, trancheSupply);
     }
 
     /// @notice Given a senior immature tranche, calculates the claimable collateral balance backing the tranche supply.
