6b2bf2a1f871d12b4e929ffe60948969d1f977b7
aalavandhann <6264334+aalavandhan@users.noreply.github.com>
2024-01-11 12:39:17 -0500

    fee policy: code review fixes
    Signed-off-by: aalavandhann <6264334+aalavandhan@users.noreply.github.com>

--- spot-contracts/contracts/_interfaces/IFeePolicy.sol
+++ spot-contracts/contracts/_interfaces/IFeePolicy.sol
@@ -5,17 +5,17 @@ import { IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC
 import { IBondController } from "./buttonwood/IBondController.sol";
 
 interface IFeePolicy {
-    /// @param sr The current system subscription ratio.
+    /// @param dr The current system deviation ratio.
     /// @return The percentage of the mint perp tokens to be charged as fees,
     ///         as a fixed-point number with {DECIMALS} decimal places.
-    function perpMintFeePerc(uint256 sr) external returns (uint256);
+    function computePerpMintFeePerc(uint256 dr) external returns (uint256);
 
-    /// @param sr The current system subscription ratio.
+    /// @param dr The current system deviation ratio.
     /// @return The percentage of the burnt perp tokens to be charged as fees,
     ///         as a fixed-point number with {DECIMALS} decimal places.
-    function perpBurnFeePerc(uint256 sr) external returns (uint256);
+    function computePerpBurnFeePerc(uint256 dr) external returns (uint256);
 
-    /// @param sr The current system subscription ratio.
+    /// @param dr The current system deviation ratio.
     /// @return The applied exchange rate adjustment between tranches into perp and
     ///         tokens out of perp during a rollover,
     ///         as a fixed-point number with {DECIMALS} decimal places.
@@ -25,33 +25,37 @@ interface IFeePolicy {
     ///         example) 100 tranchesIn for 99 tranchesOut
     ///      - A fee of -1%, implies the exchange rate is adjusted in favor of tranchesOut.
     ///         example) 99 tranchesIn for 100 tranchesOut
-    function perpRolloverFeePerc(uint256 sr) external returns (int256);
+    function computePerpRolloverFeePerc(uint256 dr) external returns (int256);
 
     /// @return The percentage of the mint vault note amount to be charged as fees,
     ///         as a fixed-point number with {DECIMALS} decimal places.
-    function vaultMintFeePerc() external returns (uint256);
+    function computeVaultMintFeePerc() external returns (uint256);
 
     /// @return The percentage of the burnt vault note amount to be charged as fees,
     ///         as a fixed-point number with {DECIMALS} decimal places.
-    function vaultBurnFeePerc() external returns (uint256);
+    function computeVaultBurnFeePerc() external returns (uint256);
 
     /// @return The fixed amount fee charged by the vault during each deployment,
     ///         denominated in the underlying collateral asset.
-    function vaultDeploymentFee() external returns (uint256);
+    function computeVaultDeploymentFee() external returns (uint256);
 
-    /// @param sr The current system subscription ratio.
+    /// @param dr The current system deviation ratio.
     /// @return perpFeePerc The percentage of perp tokens out to be charged as swap fees by perp,
     ///         as a fixed-point numbers with {DECIMALS} decimal places.
     /// @return vaultFeePerc The percentage of perp tokens out to be charged as swap fees by the vault,
     ///         as a fixed-point numbers with {DECIMALS} decimal places.
-    function underlyingToPerpSwapFeePercs(uint256 sr) external returns (uint256 perpFeePerc, uint256 vaultFeePerc);
+    function computeUnderlyingToPerpSwapFeePercs(uint256 dr)
+        external
+        returns (uint256 perpFeePerc, uint256 vaultFeePerc);
 
-    /// @param sr The current system subscription ratio.
+    /// @param dr The current system deviation ratio.
     /// @return perpFeePerc The percentage of underlying tokens out to be charged as swap fees by perp,
     ///         as a fixed-point numbers with {DECIMALS} decimal places.
     /// @return vaultFeePerc The percentage of underlying tokens out to be charged as swap fees by the vault,
     ///         as a fixed-point numbers with {DECIMALS} decimal places.
-    function perpToUnderlyingSwapFeePercs(uint256 sr) external returns (uint256 perpFeePerc, uint256 vaultFeePerc);
+    function computePerpToUnderlyingSwapFeePercs(uint256 dr)
+        external
+        returns (uint256 perpFeePerc, uint256 vaultFeePerc);
 
     /// @return Number of decimals representing a multiplier of 1.0. So, 100% = 1*10**decimals.
     function decimals() external view returns (uint8);
@@ -69,6 +73,6 @@ interface IFeePolicy {
     }
 
     /// @param s The subscription parameters of both the perp and vault systems.
-    /// @return The subscription ratio given the system subscription parameters.
-    function computeSubscriptionRatio(SubscriptionParams memory s) external returns (uint256);
+    /// @return The system deviation ratio.
+    function computeDeviationRatio(SubscriptionParams memory s) external returns (uint256);
 }
