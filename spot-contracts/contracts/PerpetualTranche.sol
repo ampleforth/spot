fc9af4688a8a75053f79d6370179eb6b77a01699
aalavandhann <6264334+aalavandhan@users.noreply.github.com>
2024-02-06 07:02:28 -0500

    made transfer fn nonReentrant

--- spot-contracts/contracts/PerpetualTranche.sol
+++ spot-contracts/contracts/PerpetualTranche.sol
@@ -315,7 +315,11 @@ contract PerpetualTranche is
     /// @param token The token address.
     /// @param to The destination address.
     /// @param amount The amount of tokens to be transferred.
-    function transferERC20(IERC20Upgradeable token, address to, uint256 amount) external afterStateUpdate onlyOwner {
+    function transferERC20(
+        IERC20Upgradeable token,
+        address to,
+        uint256 amount
+    ) external afterStateUpdate nonReentrant onlyOwner {
         if (_inReserve(token)) {
             revert UnauthorizedTransferOut();
         }
