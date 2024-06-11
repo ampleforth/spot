8eac66b2f524da286d64183ee80e17b7578f42a1
aalavandhann <6264334+aalavandhan@users.noreply.github.com>
2024-06-11 09:40:08 -0400

    interface and utility updates

--- spot-contracts/contracts/_interfaces/IRolloverVault.sol
+++ spot-contracts/contracts/_interfaces/IRolloverVault.sol
@@ -1,10 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
+import { IBalancer } from "./IBalancer.sol";
 import { IVault } from "./IVault.sol";
-import { SubscriptionParams } from "./CommonTypes.sol";
+import { ITranche } from "./buttonwood/ITranche.sol";
 
 interface IRolloverVault is IVault {
+    /// @notice Deposits the tranche tokens from {msg.sender} into the vault and mints notes.
+    /// @param trancheAmt The amount tranche tokens to be deposited into the vault.
+    /// @return The amount of notes minted.
+    function deposit(ITranche tranche, uint256 trancheAmt) external returns (uint256);
+
+    /// @notice Computes the amount of notes minted when given amount of tranche tokens
+    ///         are deposited into the system.
+    /// @param trancheAmt The amount tranche tokens to be deposited into the vault.
+    /// @return The amount of notes to be minted.
+    function computeMintAmt(ITranche tranche, uint256 trancheAmt) external returns (uint256);
+
     /// @notice Allows users to swap their underlying tokens for perps held by the vault.
     /// @param underlyingAmtIn The amount of underlying tokens swapped in.
     /// @return The amount of perp tokens swapped out.
@@ -15,23 +27,9 @@ interface IRolloverVault is IVault {
     /// @return The amount of underlying tokens swapped out.
     function swapPerpsForUnderlying(uint256 perpAmtIn) external returns (uint256);
 
-    /// @notice Computes the amount of perp tokens that are returned when user swaps a given number of underlying tokens.
-    /// @param underlyingAmtIn The number of underlying tokens the user swaps in.
-    /// @return perpAmtOut The number of perp tokens returned to the user.
-    /// @return perpFeeAmtToBurn The amount of perp tokens to be paid to the perp contract as mint fees.
-    /// @return s The pre-swap perp and vault subscription state.
-    function computeUnderlyingToPerpSwapAmt(
-        uint256 underlyingAmtIn
-    ) external returns (uint256, uint256, SubscriptionParams memory);
-
-    /// @notice Computes the amount of underlying tokens that are returned when user swaps a given number of perp tokens.
-    /// @param perpAmtIn The number of perp tokens the user swaps in.
-    /// @return underlyingAmtOut The number of underlying tokens returned to the user.
-    /// @return perpFeeAmtToBurn The amount of perp tokens to be paid to the perp contract as burn fees.
-    /// @return s The pre-swap perp and vault subscription state.
-    function computePerpToUnderlyingSwapAmt(
-        uint256 perpAmtIn
-    ) external returns (uint256, uint256, SubscriptionParams memory);
+    /// @notice The balancer contract which controls fees and orchestrates external actions with perp and vault systems.
+    /// @return Address of the balancer contract.
+    function balancer() external view returns (IBalancer);
 
     /// @return The system's current deviation ratio.
     function deviationRatio() external returns (uint256);
