64b9a363b8eee5ca8366e7117728bbf0498d087c
Aalavandhan <6264334+aalavandhan@users.noreply.github.com>
2024-11-26 12:48:27 -0500

    Deployed new oracle contract and managers (#233)
    * deployed new oracle and managers
    
    * updated readme

--- spot-vaults/tasks/info.ts
+++ spot-vaults/tasks/info.ts
@@ -7,6 +7,68 @@ function pp(val, decimals) {
   return parseFloat(ethers.formatUnits(val, decimals));
 }
 
+task("info:MetaOracle")
+  .addPositionalParam(
+    "address",
+    "the address of the meta oracle contract",
+    undefined,
+    types.string,
+    false,
+  )
+  .setAction(async function (args: TaskArguments, hre) {
+    const { address } = args;
+
+    const oracle = await hre.ethers.getContractAt("IMetaOracle", address);
+    const oracleDecimals = await oracle.decimals();
+    console.log("---------------------------------------------------------------");
+    console.log("MetaOracle:", oracle.target);
+
+    console.log("---------------------------------------------------------------");
+    const usdcPriceData = await oracle.usdcPrice.staticCall();
+    console.log("usdcPrice:", pp(usdcPriceData[0], oracleDecimals));
+
+    const ethPriceData = await oracle.ethUsdPrice.staticCall();
+    console.log("ethPrice:", pp(ethPriceData[0], oracleDecimals));
+
+    const wamplPriceData = await oracle.wamplUsdPrice.staticCall();
+    console.log("wamplPrice:", pp(wamplPriceData[0], oracleDecimals));
+
+    console.log("---------------------------------------------------------------");
+
+    const amplPriceData = await oracle.amplUsdPrice.staticCall();
+    console.log("amplPrice:", pp(amplPriceData[0], oracleDecimals));
+
+    const amplTargetPriceData = await oracle.amplTargetUsdPrice.staticCall();
+    console.log("amplTargetPrice:", pp(amplTargetPriceData[0], oracleDecimals));
+
+    const amplDeviationData = await oracle.amplPriceDeviation.staticCall();
+    console.log("amplDeviation:", pp(amplDeviationData[0], oracleDecimals));
+
+    console.log("---------------------------------------------------------------");
+
+    const spotPriceData = await oracle.spotUsdPrice.staticCall();
+    console.log("spotPrice:", pp(spotPriceData[0], oracleDecimals));
+
+    const spotFmvPriceData = await oracle.spotFmvUsdPrice.staticCall();
+    console.log("spotFmvPrice:", pp(spotFmvPriceData[0], oracleDecimals));
+
+    const spotDeviationData = await oracle.spotPriceDeviation.staticCall();
+    console.log("spotDeviation:", pp(spotDeviationData[0], oracleDecimals));
+
+    console.log("---------------------------------------------------------------");
+
+    const spotDeviation = pp(spotDeviationData[0], oracleDecimals);
+    const amplDeviation = pp(amplDeviationData[0], oracleDecimals);
+    const relativeDeviation = spotDeviation / amplDeviation;
+
+    console.log("relativeDeviation:", relativeDeviation);
+    console.log("noArbZone:", relativeDeviation > 0.9 && relativeDeviation < 1.025);
+    console.log("flashMintZone:", relativeDeviation >= 1.025);
+    console.log("flashRedeemZone:", relativeDeviation <= 0.9);
+
+    console.log("---------------------------------------------------------------");
+  });
+
 task("info:BillBroker")
   .addPositionalParam(
     "address",
@@ -31,21 +93,20 @@ task("info:BillBroker")
     const unitUsd = await billBroker.usdUnitAmt();
     const unitPerp = await billBroker.perpUnitAmt();
 
-    const pricingStrategy = await hre.ethers.getContractAt(
-      "SpotAppraiser",
-      await billBroker.pricingStrategy.staticCall(),
+    const oracle = await hre.ethers.getContractAt(
+      "SpotPricer",
+      // await billBroker.oracle.staticCall(),
+      "0x0f8f519878c10ce36C6aAF89c1AeefaaDE5D7881",
     );
-    const pricingStrategyDecimals = await pricingStrategy.decimals();
+    const oracleDecimals = await oracle.decimals();
     console.log("---------------------------------------------------------------");
-    console.log("pricingStrategy:", pricingStrategy.target);
-    console.log("owner:", await pricingStrategy.owner());
-    const usdPriceCall = await pricingStrategy.usdPrice.staticCall();
-    console.log("usdPrice:", pp(usdPriceCall[0], pricingStrategyDecimals));
+    console.log("oracle:", oracle.target);
+    const usdPriceCall = await oracle.usdPrice.staticCall();
+    console.log("usdPrice:", pp(usdPriceCall[0], oracleDecimals));
     console.log("usdPriceValid:", usdPriceCall[1]);
-    const perpPriceCall = await pricingStrategy.perpPrice.staticCall();
-    console.log("perpPrice:", pp(perpPriceCall[0], pricingStrategyDecimals));
+    const perpPriceCall = await oracle.perpUsdPrice.staticCall();
+    console.log("perpPrice:", pp(perpPriceCall[0], oracleDecimals));
     console.log("perpPriceValid:", perpPriceCall[1]);
-    console.log("isSpotHealthy:", await pricingStrategy.isSPOTHealthy.staticCall());
     console.log("---------------------------------------------------------------");
     console.log("BillBroker:", billBroker.target);
     console.log("owner:", await billBroker.owner());
@@ -143,24 +204,15 @@ task("info:WethWamplManager")
     const { address } = args;
 
     const manager = await hre.ethers.getContractAt("WethWamplManager", address);
+    const oracle = await hre.ethers.getContractAt("IMetaOracle", await manager.oracle());
     const managerDecimals = await manager.decimals();
     console.log("---------------------------------------------------------------");
     console.log("WethWamplManager:", manager.target);
     console.log("owner:", await manager.owner());
-    console.log("cpiOracle:", await manager.cpiOracle());
-    console.log("ethOracle:", await manager.ethOracle());
+    console.log("oracle:", oracle.target);
 
     console.log("---------------------------------------------------------------");
-    const ethPriceData = await manager.getEthUSDPrice();
-    console.log("ethPrice:", pp(ethPriceData[0], managerDecimals));
-
-    const wamplPrice = await manager.getWamplUSDPrice(ethPriceData[0]);
-    console.log("wamplPrice:", pp(wamplPrice, managerDecimals));
-
-    const amplPrice = await manager.getAmplUSDPrice(ethPriceData[0]);
-    console.log("amplPrice:", pp(amplPrice, managerDecimals));
-
-    const r = await manager.computeDeviationFactor.staticCall();
+    const r = await oracle.amplPriceDeviation.staticCall();
     const deviation = r[0];
     console.log("dataValid:", r[1]);
     console.log("isOverweightWampl:", await manager.isOverweightWampl());
@@ -193,22 +245,25 @@ task("info:UsdcSpotManager")
     const { address } = args;
 
     const manager = await hre.ethers.getContractAt("UsdcSpotManager", address);
+    const oracle = await hre.ethers.getContractAt("IMetaOracle", await manager.oracle());
     const managerDecimals = await manager.decimals();
     console.log("---------------------------------------------------------------");
     console.log("UsdcSpotManager:", manager.target);
     console.log("owner:", await manager.owner());
+    console.log("oracle:", oracle.target);
 
     console.log("---------------------------------------------------------------");
-    const spotPrice = await manager.getSpotUSDPrice();
-    console.log("spotPrice:", pp(spotPrice, managerDecimals));
-
-    const r = await manager.computeDeviationFactor.staticCall();
+    const r = await oracle.spotPriceDeviation.staticCall();
     const deviation = r[0];
     console.log("dataValid:", r[1]);
     console.log("isOverweightSpot:", await manager.isOverweightSpot());
-    console.log("prevDeviation:", pp(await manager.prevDeviation(), managerDecimals));
+    console.log("prevWithinActiveZone:", await manager.prevWithinActiveZone());
+    console.log("withinActiveZone:", await manager.activeZone(deviation));
     console.log("deviation:", pp(deviation, managerDecimals));
-
+    console.log(
+      "fullRangePerc:",
+      pp(await manager.activeFullRangePerc(), managerDecimals),
+    );
     let rebalanceActive = true;
     try {
       await manager.rebalance.staticCall();
