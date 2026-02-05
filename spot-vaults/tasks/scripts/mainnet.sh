fdebc3f3b88bb766f6d53dfba8b87a8574aae170
aalavandhann <6264334+aalavandhan@users.noreply.github.com>
2026-02-05 13:57:34 -0500

    dr vault testnet and mainnet deployment

--- spot-vaults/tasks/scripts/mainnet.sh
+++ spot-vaults/tasks/scripts/mainnet.sh
@@ -24,6 +24,13 @@ yarn hardhat --network mainnet deploy:CharmManager \
 	--vault "0x2dcaff0f75765d7867887fc402b71c841b3a4bfb" \
 	--oracle "0x0f8f519878c10ce36C6aAF89c1AeefaaDE5D7881"
 
+yarn hardhat --network mainnet deploy:DRBalancerVault \
+	--name "DR Balancer Vault" \
+	--symbol "DR-VAULT" \
+	--underlying "0xD46bA6D942050d489DBd938a2C909A5d5039A161" \
+	--perp "0xC1f33e0cf7e40a67375007104B929E49a581bafE" \
+	--rollover-vault "0x82A91a0D599A45d8E9Af781D67f695d7C72869Bd"
+
 yarn hardhat --network mainnet transferOwnership "0xA088Aef966CAD7fE0B38e28c2E07590127Ab4ccB" \
   --new-owner-address "0x57981B1EaFe4b18EC97f8B10859B40207b364662"
 
@@ -33,11 +40,18 @@ yarn hardhat --network mainnet transferOwnership "0x574fca658b4B59E965C0e5f74761
 yarn hardhat --network mainnet transferOwnership "0x2f67158859Fe0f69f5773570eC60444Fe0c1693c" \
   --new-owner-address "0x57981B1EaFe4b18EC97f8B10859B40207b364662"
 
+yarn hardhat --network mainnet transferOwnership "0xd6e88D952ea0B1dFa42018c81eb597b3C1e2BF48" \
+  --new-owner-address "0x57981B1EaFe4b18EC97f8B10859B40207b364662"
+
+yarn hardhat --network mainnet transferOwnership "0x6f60625c5B4Bdf89b9F18B9c681310E6B3dAcDbD" \
+  --new-owner-address "0x57981B1EaFe4b18EC97f8B10859B40207b364662"
+
 ########################################################################
 ## INFO
 
 yarn hardhat --network mainnet info:MetaOracle "0x0f8f519878c10ce36C6aAF89c1AeefaaDE5D7881"
 yarn hardhat --network mainnet info:BillBroker "0xA088Aef966CAD7fE0B38e28c2E07590127Ab4ccB"
+yarn hardhat --network mainnet info:DRBalancerVault "0x6f60625c5B4Bdf89b9F18B9c681310E6B3dAcDbD"
 yarn hardhat --network mainnet info:WethWamplManager "0x574fca658b4B59E965C0e5f74761AE0Ac41DA6a7"
 yarn hardhat --network mainnet info:UsdcSpotManager "0x2f67158859Fe0f69f5773570eC60444Fe0c1693c"
 
@@ -60,6 +74,14 @@ yarn hardhat --network mainnet ops:swapPerpsForUSD \
 yarn hardhat --network mainnet ops:redeem \
 	--address "0xA088Aef966CAD7fE0B38e28c2E07590127Ab4ccB" \
 	--amount 1000
+
+yarn hardhat --network mainnet ops:drVaultDeposit \
+	--address "<DR_BALANCER_VAULT_ADDRESS>" \
+	--underlying-amount 1000 \
+	--perp-amount 1000
+
+yarn hardhat --network mainnet ops:drVaultRebalance \
+	--address "<DR_BALANCER_VAULT_ADDRESS>"
 
 ########################################################################
 ## Upgrade
